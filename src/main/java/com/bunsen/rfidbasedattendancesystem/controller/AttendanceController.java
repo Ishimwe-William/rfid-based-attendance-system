246715b76a17fc791d0873260995849444aa99fd
Ishimwe-William <ishimwe.william2000@gmail.com>
2023-11-29 13:21:01 +0200

    ESP8266 connection status added

--- src/main/java/com/bunsen/rfidbasedattendancesystem/controller/AttendanceController.java
+++ src/main/java/com/bunsen/rfidbasedattendancesystem/controller/AttendanceController.java
@@ -12,28 +12,68 @@ import java.util.List;
 @Controller
 public class AttendanceController {
     private AttendanceService attendanceService;
+    private boolean arduinoConnected = false; // Flag to track Arduino connection status
+    private long lastArduinoCommunicationTime = System.currentTimeMillis();
+    private static final long MAX_INACTIVITY_PERIOD = 1000; // Maximum inactivity period in milliseconds (adjust as needed)
 
     @Autowired
     public AttendanceController(AttendanceService attendanceService) {
         this.attendanceService = attendanceService;
     }
+
     @GetMapping("/")
     public String index(Model model) {
         List<Attendance> data = attendanceService.getAllAttendancesSortedByCreated();
         model.addAttribute("attendances", data);
+        updateConnectionStatus();
+
+        // Convert arduinoConnected to a string attribute
+        String connectionStatus = arduinoConnected ? "Connected" : "Not Connected";
+
+        model.addAttribute("arduinoConnected", connectionStatus);
+        model.addAttribute("connectionColor", arduinoConnected ? "green" : "red");
         return "index";
     }
+
 
     @PostMapping("/sensor-data")
     public String postData(@RequestParam("card_number") String card_no) {
         Attendance attendance = new Attendance();
-
         if (card_no != null) {
-            attendance.setCardNo(card_no);
+            attendance.setCardNo(card_no.toUpperCase());
             attendanceService.saveData(attendance);
             return "success";
         } else {
             return "error";
         }
+    }
+
+    @PostMapping("/connection-status")
+    @ResponseBody
+    public String updateArduinoConnectionStatus(@RequestBody String status) {
+        if ("connected".equalsIgnoreCase(status.trim())) {
+            arduinoConnected = true;
+            lastArduinoCommunicationTime = System.currentTimeMillis();
+            return "Connection status updated: Connected";
+        } else {
+            arduinoConnected = false;
+            return "Connection status updated: Not Connected";
+        }
+    }
+
+    private void updateConnectionStatus() {
+        long currentTime = System.currentTimeMillis();
+        if (currentTime - lastArduinoCommunicationTime > MAX_INACTIVITY_PERIOD) {
+            // The Arduino has not communicated for a while, mark it as not connected
+            arduinoConnected = false;
+        }
+    }
+
+    // New method for handling the AJAX request to get connection status
+    @GetMapping("/connection-status")
+    @ResponseBody
+    public String getArduinoConnectionStatus() {
+        updateConnectionStatus();
+        return arduinoConnected ? "Connected" : "Not Connected";
     }
 }
